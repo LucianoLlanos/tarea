cd193b450b99846b611487d4b986f7c53dfd6882
tunombre <email>
2023-12-04 17:43:48 -0300

    feat(App.jsx): se corrigió props del componente card.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Card from './components/general/Card/Card'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello Wolrd </h1>
+    <>
+      <h1>Hello Wolrd</h1>
+      <Card title="My friend"/>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Swal from 'sweetalert2'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
